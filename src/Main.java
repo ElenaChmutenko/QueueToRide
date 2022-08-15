cfb8d0ebd8d7bb8c8ef27f3b7dece540d8ea46c1
chmutenkoa <alenachm@gmail.com>
2022-08-16 01:14:56 +0300

    Ticket check added

--- src/Main.java
+++ src/Main.java
@@ -8,21 +8,30 @@ public class Main {
         Queue <Person> queue = new LinkedList<>(generateClients());
 
         while (!queue.isEmpty()) {
-
-            Person p = queue.peek();
-            System.out.println(p);
-            p.decreaseTicketsN();
-            if (p.ifTicketsLeft()) {
-                queue.add(p);
+            // pulls first person out of the queue
+            Person person = queue.peek();
+            // ticket check: if person has no ticket, person leaves the queue
+            if (!person.ifTicketsLeft()) {
+                queue.remove(person);
+                continue;
             }
-            queue.remove(p);
+            person.decreaseTicketsN();
+            StringBuilder sb = new StringBuilder(person.toString());
+            sb.append(" has ridden attraction");
+            System.out.println(sb.toString());
+            // if the person still has tickets, added to the end of the queue
+            if (person.ifTicketsLeft()) {
+                queue.add(person);
+            }
+            // person leaves the queue
+            queue.remove(person);
         }
     }
 
     public static List<Person> generateClients() {
         List<Person> clients = new LinkedList<>();
         clients.add(new Person("Bruce", "Allmighty", 6));
-        clients.add(new Person("Denis", "Kirienko", 4));
+        clients.add(new Person("Denis", "Kirienko", 0));
         clients.add(new Person("Ivan", "Proc", 10));
         clients.add(new Person("Dimsas", "Milky", 9));
         clients.add(new Person("Vavi", "Maloy", 7));
